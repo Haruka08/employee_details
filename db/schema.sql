2fde32f4a0443251da104af99ed118c4d8c4bc74
Haruka08 <haruka.hashimoto08@gmail.com>
2022-09-24 15:58:22 +0100

    updated index.js file to include inquirer prompts

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,26 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE departments (
-  id INT NOT NULL,
-  department VARCHAR(30),
-  PRIMARY KEY (id)
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  department VARCHAR(30)
 );
 
 CREATE TABLE roles (
-  id INT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
   department_id INT,
-  -- PRIMARY KEY(id),
-  FOREIGN KEY (department_id),
-  REFERENCES departments(id),
+  FOREIGN KEY (department_id)
+  REFERENCES departments(id)
   ON DELETE SET NULL
 );
 
--- CREATE TABLE employees (
---   id INT NOT NULL,
---   first_name VARCHAR(30) NOT NULL,
---   last_name VARCHAR(30) NOT NULL,
---   role_id INT,
---   manager_id INT,
---   FOREIGN KEY(role_id),
---   REFERENCES roles(id)
--- );
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY(role_id)
+  REFERENCES roles(id)
+);
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // Packages and files required for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+let db = require('./db/connection.js')
 
-const employees = [];
-
-const Question = [
+const selectAction = [
     {
         type: "list",
         message: "What would you like to do?",
-        name: "jobRole",
+        name: "action",
         choices:[
             "view all departments", 
             "view all roles", 
@@ -16,25 +15,78 @@ const Question = [
             "add a department", 
             "add a role", 
             "add an employee",
-            "update an employee role"
+            "update an employee role",
+            "Exit"
             ]
     }
 ];
 
-function createManager(name, id, email, officeNum) {
-    const newManager = new Manager(name, id, email, officeNum);
-    employees.push(newManager);
-    mainQuestion();
-}
+function action() {
+    inquirer.prompt(
+        selectAction
+    ).then ((data) => {
+        if (data == "view all departments"){
+            viewAllDepartments()
+        } else if (data == "view all roles"){
+            viewAllRoles()
+        } else if (data == "view all employees"){
+            viewAllEmployees()
+        } else if (data == "add a department"){
+            addDepartment()
+        } else if (data == "add a role"){
+            addRole()
+        } else if (data == "add a employee"){
+            addEmployee()
+        } else if (data == "update an employee role"){
+            updateEmployeeRole()
+        } else {
+            return;
+        }
+    })
+};
 
-function createEngineer(name, id, email, gitHubUser) {
-    const newEngineer = new Engineer(name, id, email, gitHubUser);
-    employees.push(newEngineer);
-    mainQuestion();
-}
+function viewAllDepartments() {
+    db.query('SELECT * FROM roles JOIN departments ON roles.department_id = departments.id; SELECT * FROM employees JOIN roles ON employees.role_id = roles.id;', function (err, results) {
+        console.log(results);
+    })
+};
 
-function createIntern(name, id, email, school) {
-    const newIntern = new Intern(name, id, email, school);
-    employees.push(newIntern);
-    mainQuestion();
-}
+// function viewAllRoles(name, id, email, gitHubUser) {
+//     const newEngineer = new Engineer(name, id, email, gitHubUser);
+//     employees.push(newEngineer);
+//     Question();
+// }
+
+// function viewAllEmployees(name, id, email, school) {
+//     const newIntern = new Intern(name, id, email, school);
+//     employees.push(newIntern);
+//     Question();
+// }
+
+// function addDepartment(name, id, email, school) {
+//     const newIntern = new Intern(name, id, email, school);
+//     employees.push(newIntern);
+//     Question();
+// }
+
+// function addRole(name, id, email, school) {
+//     const newIntern = new Intern(name, id, email, school);
+//     employees.push(newIntern);
+//     Question();
+// }
+
+// function allEmployee(name, id, email, school) {
+//     const newIntern = new Intern(name, id, email, school);
+//     employees.push(newIntern);
+//     Question();
+// }
+
+// function updateEmployeeRole(name, id, email, school) {
+//     const newIntern = new Intern(name, id, email, school);
+//     employees.push(newIntern);
+//     Question();
+// }
+
+
+
+action();
